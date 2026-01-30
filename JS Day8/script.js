d5df8c59316b5f2b881746b5a78066eb02929f16
absolute-one <raj.ansh0811@gmail.com>
2026-01-30 15:52:28 +0530

    commit

---
+++ JS Day8/script.js
@@ -0,0 +1,16 @@
+// @ 30 Jan 
+
+const heading = document.getElementById("myHeading");
+console.log(heading);
+
+heading.style.color= "blue";
+heading.style.backgroundColor= "grey";
+console.log(heading.textContent);
+heading.textContent= "DOM and it's introduction!.";
+
+const subHeading = document.getElementsByClassName("sub-heading")
+console.log(subHeading);
+
+for(let i = 0; i<subHeading.length;i++) {
+    subHeading[i].style.color="blue";
+}
