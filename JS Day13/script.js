e38792a3a87dea84cef95a40ab769bf13b7fec22
absolute-one <raj.ansh0811@gmail.com>
2026-02-25 10:55:31 +0530

    day13

--- JS Day13/script.js
+++ JS Day13/script.js
@@ -1,14 +1,32 @@
 // Synchronus and Asynchronus 
 
-console.log("start...");
+// console.log("start...");
 
-function myFunction(cb){
-    setTimeout(()=>{
-        cb("my function timeout");
-    }, 2000);
-}
-myFunction((data)=>{
-    console.log(data);
-});
+// function myFunction(cb){
+//     setTimeout(()=>{
+//         cb("my function timeout");
+//     }, 2000);
+// }
+// myFunction((data)=>{
+//     console.log(data);
+// });
 
-console.log("end...")
+// console.log("end...")
+
+// mixture of asynchronus and synchronus
+
+console.log("Let's make maggie yayy!!!")
+
+setTimeout(()=>{
+    console.log("Maggieee bannn gyiiiiiiiii!!")
+}, 5000)
+
+setTimeout(()=>{
+    console.log("Bas ban hi gyi almost...")
+},3500)
+
+setTimeout(()=>{
+    console.log("Masala daal diya...")
+},2000)
+
+console.log("Paani boil kr diya..")
